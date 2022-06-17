ed810bfa5f6e881bd2399356a6a6cb89338b771a
Asus <Asus@LAPTOP-V605S04R>
2022-06-17 21:26:59 +0300

    Добавьте файлы проекта.

---
+++ PracticalWork_1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+        }
+    }
+}
